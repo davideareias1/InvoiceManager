9221eb3dcbcf7fb6e4e135ad2907e7eb58f35634
Davide Areias <davideareias1@gmail.com>
2025-09-12 11:46:11 -0700

    chore(ddd): start ui-minimal-ddd branch, remove existing UI, add minimal layout and page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,6 @@ import './globals.css'
 import { FileSystemProvider } from '../contexts/FileSystemContext'
 import { CompanyProvider } from '../contexts/CompanyContext'
 import { GoogleDriveProvider } from '../contexts/GoogleDriveContext'
-import { MainNav } from '@/components/main-nav'
-import { Toaster } from '@/components/ui/toaster'
-import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,23 +20,14 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
-                {/* We'll load scripts dynamically in GoogleDriveContext */}
             </head>
             <body className={inter.className}>
                 <FileSystemProvider>
                     <GoogleDriveProvider>
                         <CompanyProvider>
-                            <div className="flex min-h-screen flex-col">
-                                <div className="border-b">
-                                    <div className="container flex h-16 items-center px-4">
-                                        <MainNav />
-                                    </div>
-                                </div>
-                                <main className="flex-1 container py-6 px-4">
-                                    {children}
-                                </main>
-                            </div>
-                            <Toaster />
+                            <main>
+                                {children}
+                            </main>
                         </CompanyProvider>
                     </GoogleDriveProvider>
                 </FileSystemProvider>
